803b2068e2ea66f4ad3825efb26708eec89a8c3a
italoandrez <72633376+italoandrez@users.noreply.github.com>
2021-03-20 16:38:15 -0300

    Create 20210320143139-createTableTasks.js

---
+++ src/database/migrations/20210320143139-createTableTasks.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable("tasks", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      descricao: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.ENUM("pendente", "andamento", "concluida"),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("tasks");
+  },
+};
